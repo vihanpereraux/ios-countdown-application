4f899cfb997d0f497d178cee77082604ce09defd
Vihan Perera <vihanpereraux@gmail.com>
2023-07-06 22:33:40 +0530

    Initial Commit

---
+++ countdown-application/countdown_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdown_applicationApp.swift
+//  countdown-application
+//
+//  Created by Vihan Perera on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct countdown_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
